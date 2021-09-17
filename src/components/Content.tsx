ca238f213e725e60dd51f7714bc8599818104079
OldKode <lu.joao.lima@gmail.com>
2021-09-16 21:53:57 -0300

    desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
+//imports
+import { GenreResponseProps, MovieProps } from "../App";
+import { Header } from "./Header";
+
+import { MovieCard } from "./MovieCard";
+
+// interfaces
+interface ContentPros {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function Content({ selectedGenre, movies}: ContentPros) {
   // Complete aqui
+
+  return (
+        <div className="container">
+          <Header title = {selectedGenre.title} />
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard 
+                key ={movie.imdbID} 
+                title={movie.Title} 
+                poster={movie.Poster} 
+                runtime={movie.Runtime} 
+                rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
